5faa1c010b865e1c31f83f734a447185a94e37c4
128na <simutrans.128na@gmail.com>
2017-11-18 18:44:04 +0900

    ツイート機能を実装

--- src/resources/views/search.blade.php
+++ src/resources/views/search.blade.php
@@ -9,6 +9,7 @@
     <tr>
       <th>サイト名</th>
       <th>ページ名</th>
+      <th>Pak</th>
       <th>一致テキスト</th>
       <th>最終更新日</th>
     </tr>
@@ -18,18 +19,30 @@
     <tr>
       <td>{{ $page->site_name }}</td>
       <td><a href="{{ $page->url }}" target="_blank"><span class="highlightable">{{ $page->title }}</span></a></td>
+      <td>{{ $page->pak }}</td>
       <td><span class="highlightable">{{ $page->expectText($word) }}</span></td>
       <td>{{ $page->updated_at }}</td>
     </tr>
   </tbody>
 @endforeach
 </table>
+<p>検索結果をツイートできます</p>
+<a
+  href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+  class="twitter-share-button"
+  data-size="large"
+  data-text="{{ implode('、',$conds).'での検索結果' }} | {{ config('const.app.name') }}"
+  data-url="{{ url()->full() }}"
+  data-hashtags="simutrans_cross_search"
+  data-show-count="false"
+>Tweet</a>
 @else
   <span>ないです</span>
 @endif
 @endsection
 
 @section('script')
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 <script>
   $(function() {
     const word = '{{ $word }}'
