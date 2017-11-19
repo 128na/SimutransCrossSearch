4cb199eabdda10ca71270f511232c19f8a117a0b
128na <simutrans.128na@gmail.com>
2017-11-19 16:19:55 +0900

    ajax調整

--- src/resources/assets/js/services/api.js
+++ src/resources/assets/js/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = window.base_url || '/';
+axios.defaults.baseURL = window.base_url;
+axios.defaults.timeout = 2500;
 
 export default {
   fetchRss() {
-    return axios.get('/api/rss')
+    return axios.get('api/rss')
   },
 
 }
