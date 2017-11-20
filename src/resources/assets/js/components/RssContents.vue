22fc9d4f97387db1ff0f502da38e144fadd52e1a
128na <simutrans.128na@gmail.com>
2017-11-20 21:00:23 +0900

    RSSフィールドに日数区分を追加

---
+++ src/resources/assets/js/components/RssContents.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul v-show="hasContents">
+    <li class="label-date"><slot>日付</slot></li>
+    <li
+      v-for="(item, idx) in contents"
+      :key="idx"
+    >
+      <span>[{{ item.time.format('YYYY/MM/DD HH:mm') }}]</span>
+      <a :href="item.link" target="_blank">{{ item.title }}</a>
+      <span>(<a :href="item.site.url" target="_blank">{{ item.site.name }}</a>)</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    contents: Array,
+  },
+  computed: {
+    hasContents() {
+      return this.contents.length > 0
+    }
+  }
+}
+</script>
