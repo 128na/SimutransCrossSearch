5bc217e0f84e97359acf1ff5e9800229beed3f77
128na <simutrans.128na@gmail.com>
2020-04-24 21:02:25 +0900

    ルーティングを修正

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', 'FrontController@index')->name('index');
-Route::get('search', 'FrontController@search')->name('search');
+Route::get('/search', 'FrontController@search')->name('search');
 
-Route::get('sitemap', 'SitemapController@index')->name('sitemap');
-Route::get('logs/schedule', 'ScheduleLogController@index')->name('logs.schedule');
-Route::get('logs/search', 'SearchLogController@index')->name('logs.search');
+Route::get('/sitemap', 'SitemapController@index')->name('sitemap');
+Route::get('/logs/schedule', 'ScheduleLogController@index')->name('logs.schedule');
+Route::get('/logs/search', 'SearchLogController@index')->name('logs.search');
