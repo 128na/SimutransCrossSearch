3f25871477761a04353614d7d7c937156599666d
128na <simutrans.128na@gmail.com>
2020-04-24 20:49:22 +0900

    最終更新日のデフォルト値を修正

--- database/migrations/2020_04_24_201345_add_last_modified_column_in_pages_table.php
+++ database/migrations/2020_04_24_201345_add_last_modified_column_in_pages_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -14,7 +15,7 @@ class AddLastModifiedColumnInPagesTable extends Migration
     public function up()
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->timestamp('last_modified')->default('1970-01-01 00:00:00')->after('title')->comment('元記事の最終更新日時');
+            $table->timestamp('last_modified')->default(new Expression('CURRENT_TIMESTAMP'))->after('title')->comment('元記事の最終更新日時');
             $table->index(['last_modified']);
         });
     }
