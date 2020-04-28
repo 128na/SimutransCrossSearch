2b8cff1325a1cf22a798ef5b917de83f169d0f4b
128na <simutrans.128na@gmail.com>
2020-04-28 09:23:00 +0900

    SPレイアウトを調整

--- resources/views/pages/table.blade.php
+++ resources/views/pages/table.blade.php
@@ -1,17 +1,17 @@
 <div class="items">
-    <div class="row p-2 border-bottom">
-        <span class="col-6 col-sm-2"><strong>日付</strong></span>
-        <span class="col-6 col-sm-2"><strong>Pakサイズ</strong></span>
-        <span class="col-6 col-sm-5"><strong>ページ名</strong></span>
-        <span class="col-6 col-sm-3"><strong>サイト名</strong></span>
+    <div class="header row p-2 border-bottom">
+        <span class="col-sm-2"><strong>日付</strong></span>
+        <span class="col-sm-2"><strong>Pakサイズ</strong></span>
+        <span class="col-sm-5"><strong>ページ名</strong></span>
+        <span class="col-sm-3"><strong>サイト名</strong></span>
     </div>
 
     @foreach ($pages as $page)
         <div class="row p-2 border-bottom">
-            <span class="col-6 col-sm-2">{{ $page->last_modified->format('Y/m/d') }}</span>
-            <span class="col-6 col-sm-2">{{ $page->paks->pluck('name')->implode(', ') }}</span>
-            <span class="col-6 col-sm-5"><a target="_blank" rel="noopener noreferrer" class="link-page" href="{{ $page->url }}">{{ $page->title }}</a></span>
-            <span class="col-6 col-sm-3"><a target="_blank" rel="noopener noreferrer" class="link-site" href="{{ $page->site_url }}">{{ $page->display_site_name }}</a></span>
+            <span class="col-sm-2">{{ $page->last_modified->format('Y/m/d') }}</span>
+            <span class="col-sm-2">{{ $page->paks->pluck('name')->implode(', ') }}</span>
+            <span class="col-sm-5"><a target="_blank" rel="noopener noreferrer" class="link-page" href="{{ $page->url }}">{{ $page->title }}</a></span>
+            <span class="col-sm-3"><a target="_blank" rel="noopener noreferrer" class="link-site" href="{{ $page->site_url }}">{{ $page->display_site_name }}</a></span>
         </div>
     @endforeach
 </div>
