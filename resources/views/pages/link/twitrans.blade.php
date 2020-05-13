5636763a0318995006e44376ddca28b5d246d382
128na <simutrans.128na@gmail.com>
2020-05-13 21:54:58 +0900

    word未指定の場合エラーになるのを修正

--- resources/views/pages/link/twitrans.blade.php
+++ resources/views/pages/link/twitrans.blade.php
@@ -1,4 +1,4 @@
 @php
-    $url = "{$page->url}?word={$word}";
+    $url = isset($word) ? "{$page->url}?word={$word}" : $page->url;
 @endphp
 <a target="_blank" rel="noopener noreferrer" class="link-page" href="{{ $url }}">{{ $page->title }}</a>
