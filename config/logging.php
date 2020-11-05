b6b625348c1a61d862f7d7f6ac5d7013bb44bd1b
128na <simutrans.128na@gmail.com>
2020-11-05 14:52:51 +0900

    ログユーザー名を変更

--- config/logging.php
+++ config/logging.php
@@ -57,7 +57,7 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
+            'username' => 'SimutransCrossSearch',
             'emoji' => ':boom:',
             'level' => 'info',
         ],
