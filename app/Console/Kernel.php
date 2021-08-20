9be0b2ef42bb997847b84fbe1e77ce79bdbfc8f5
128na <simutrans.128na@gmail.com>
2021-08-20 20:51:27 +0900

    デプロイ設定戻し

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -59,12 +59,12 @@ class Kernel extends ConsoleKernel
             ->withoutOverlapping()
             ->onOneServer();
 
-        $schedule->command('backup:clean')->dailyAt('5:00')
-            ->withoutOverlapping()
-            ->onOneServer();
-        $schedule->command('backup:run')->dailyAt('6:00')
-            ->withoutOverlapping()
-            ->onOneServer();
+        // $schedule->command('backup:clean')->dailyAt('5:00')
+        //     ->withoutOverlapping()
+        //     ->onOneServer();
+        // $schedule->command('backup:run')->dailyAt('6:00')
+        //     ->withoutOverlapping()
+        //     ->onOneServer();
 
         // 1 min APIデータ更新は毎日5時
         $schedule->command('media:fetch nico')->dailyAt('6:00')
