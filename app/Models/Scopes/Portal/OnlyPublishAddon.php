453ee14621fc889d6509c52573492b75c054c182
128na <simutrans.128na@gmail.com>
2024-04-03 23:56:58 +0900

    pint

--- app/Models/Scopes/Portal/OnlyPublishAddon.php
+++ app/Models/Scopes/Portal/OnlyPublishAddon.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models\Scopes\Portal;
 
 use App\Enums\Portal\ArticlePostType;
--- tests/Feature/Console/Commands/Pages/ScrapeCommandTest.php
+++ tests/Feature/Console/Commands/Pages/ScrapeCommandTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Console\Commands\Pages;
 
 use Tests\TestCase;
