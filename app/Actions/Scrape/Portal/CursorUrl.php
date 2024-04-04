cf9edf35aa338f52d6157784c06f3e1e2f931476
128na <simutrans.128na@gmail.com>
2024-04-05 00:23:16 +0900

    po

---
+++ app/Actions/Scrape/Portal/CursorUrl.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Scrape\Portal;
+
+use App\Models\Portal\Article;
+use Illuminate\Support\LazyCollection;
+
+class CursorUrl
+{
+    private const TOP_URL = 'https://simutrans-portal.128-bit.net';
+
+    /**
+     * @return LazyCollection<int,string>
+     */
+    public function __invoke(): LazyCollection
+    {
+        return Article::select('id')
+            ->cursor()
+            ->map(fn (Article $article): string => sprintf('%s/articles/%s', self::TOP_URL, $article->id));
+    }
+}
--- app/Actions/Scrape/Portal/ScrapeHandler.php
+++ app/Actions/Scrape/Portal/ScrapeHandler.php
@@ -5,11 +5,26 @@ declare(strict_types=1);
 namespace App\Actions\Scrape\Portal;
 
 use App\Actions\Scrape\ScrapeHandlerInterface;
+use App\Actions\Scrape\UpdateOrCreateRawPage;
+use App\Enums\SiteName;
 
 class ScrapeHandler implements ScrapeHandlerInterface
 {
+    public function __construct(
+        private readonly CursorUrl $cursorUrl,
+        private readonly UpdateOrCreateRawPage $updateOrCreateRawPage,
+    ) {
+
+    }
+
     public function __invoke(): void
     {
-
+        foreach (($this->cursorUrl)() as $url) {
+            ($this->updateOrCreateRawPage)(
+                $url,
+                SiteName::Portal,
+                ''
+            );
+        }
     }
 }
