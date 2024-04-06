87638fe3020a58afe72901fd4f6b97e63bbce7cf
128na <simutrans.128na@gmail.com>
2024-04-06 17:26:39 +0900

    rectorの対象からconfigを除外

--- rector.php
+++ rector.php
@@ -13,7 +13,6 @@ return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
         __DIR__.'/bootstrap',
-        __DIR__.'/config',
         __DIR__.'/public',
         __DIR__.'/resources',
         __DIR__.'/routes',
