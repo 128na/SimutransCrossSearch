eaae0bbbf6a63a353cbade41c3bd709c94b20562
128na <simutrans.128na@gmail.com>
2024-04-07 13:36:18 +0900

    custom pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <title>{{ $title ?? config('app.name') }}</title>
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
+    <body class="dark:bg-gray-900 ">
         @include('header')
         <section class="px-4 lg:px-6 py-2.5 my-2.5">
             <div class="mx-auto max-w-screen-md">
