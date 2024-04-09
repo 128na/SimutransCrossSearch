8390c56a7e2c36c2bdb7b8ded85e68ddcb2cb7f0
128na <simutrans.128na@gmail.com>
2024-04-10 01:51:12 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:page-scrape')->dailyAt('1:00')->withoutOverlapping()->onOneServer();
-Schedule::command('app:page-extract')->dailyAt('3:00')->withoutOverlapping()->onOneServer();
+Schedule::command('app:scrape')->dailyAt('1:00')->withoutOverlapping()->onOneServer();
+Schedule::command('app:extract')->dailyAt('3:00')->withoutOverlapping()->onOneServer();
 
 Schedule::command('app:notion-sync')->dailyAt('7:00')->withoutOverlapping()->onOneServer();
