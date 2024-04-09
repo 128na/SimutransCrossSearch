defda24220bdb8b53c0a6c760f68777b0a91b6ce
128na <simutrans.128na@gmail.com>
2024-04-09 17:28:13 +0900

    更新タイミングを調整

--- app/Actions/SyncNotion/SyncAction.php
+++ app/Actions/SyncNotion/SyncAction.php
@@ -65,14 +65,15 @@ final readonly class SyncAction
     {
         $options = $this->getOptions($database);
         foreach ($pages as $page) {
+            $exists = true;
             $url = $page->url;
             $np = $notionPages->first(fn (NotionPage $np): bool => $this->getUrlProp($np) === $url);
             if (! $np) {
-                logger('[NotionService]create', ['url' => $url]);
-                $np = $this->notion->pages()->create(NotionPage::create(PageParent::database($database->id)));
+                $exists = false;
+                $np = NotionPage::create(PageParent::database($database->id));
             }
 
-            $np->changeTitle($page->title)
+            $np = $np->changeTitle($page->title)
                 ->addProperty(
                     self::PAGE_PROP_MAPPING['url'],
                     Url::create($page->url)
@@ -95,7 +96,13 @@ final readonly class SyncAction
                     )
                 );
 
-            $this->notion->pages()->update($np);
+            if ($exists) {
+                logger('[NotionService] update', ['url' => $url]);
+                $this->notion->pages()->update($np);
+            } else {
+                logger('[NotionService] create', ['url' => $url]);
+                $this->notion->pages()->create($np);
+            }
         }
     }
 
@@ -113,7 +120,7 @@ final readonly class SyncAction
     private function getOptions(Database $database): Collection
     {
         $property = $database->properties()->get(self::PAGE_PROP_MAPPING['paks']);
-        assert($property instanceof \Notion\Pages\Properties\MultiSelect);
+        assert($property instanceof \Notion\Databases\Properties\MultiSelect);
 
         return collect($property->options);
     }
