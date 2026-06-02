c54881295186ef171f4af1b32fa89360e44cd5a2
128na <simutrans.128na@gmail.com>
2026-06-02 22:32:27 +0900

    Mock PortalHandler in tests to avoid QueryException on unmigrated portal db in CI

--- tests/Feature/Actions/Scrape/ScrapeActionTest.php
+++ tests/Feature/Actions/Scrape/ScrapeActionTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Actions\Scrape;
 
+use App\Actions\Scrape\HandlerInterface;
+use App\Actions\Scrape\Portal\Handler;
 use App\Actions\Scrape\ScrapeAction;
 use App\Enums\SiteName;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Tests\Feature\TestCase;
 
@@ -20,6 +23,14 @@ final class ScrapeActionTest extends TestCase
         Http::fake([
             '*' => Http::response('<html><body></body></html>', 200),
         ]);
+
+        // Mock PortalHandler to avoid database queries in CI where the portal connection is unmigrated
+        $this->app->bind(Handler::class, function () {
+            return new class implements HandlerInterface
+            {
+                public function __invoke(LoggerInterface $logger): void {}
+            };
+        });
 
         $scrapeAction = app(ScrapeAction::class);
         $scrapeAction(null, new NullLogger);
