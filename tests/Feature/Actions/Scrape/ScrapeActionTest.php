a52e2dbd30a252ee79dab62c33cf9f9f39b57ae8
128na <simutrans.128na@gmail.com>
2026-06-02 18:02:06 +0900

    Add tests for Scrape Action

---
+++ tests/Feature/Actions/Scrape/ScrapeActionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Actions\Scrape;
+
+use App\Actions\Scrape\ScrapeAction;
+use App\Enums\SiteName;
+use Illuminate\Support\Facades\Http;
+use Psr\Log\NullLogger;
+use Tests\Feature\TestCase;
+
+final class ScrapeActionTest extends TestCase
+{
+    public function test_invokes_handlers_for_all_sites_when_null_provided(): void
+    {
+        Http::fake([
+            '*' => Http::response('<html><body></body></html>', 200),
+        ]);
+
+        $action = app(ScrapeAction::class);
+        $action(null, new NullLogger);
+
+        // Just ensure no exceptions were thrown
+        $this->assertTrue(true);
+    }
+
+    public function test_invokes_specific_handler_when_site_provided(): void
+    {
+        Http::fake([
+            '*' => Http::response('<html><body></body></html>', 200),
+        ]);
+
+        $action = app(ScrapeAction::class);
+        $action(SiteName::Japan, new NullLogger);
+
+        $this->assertTrue(true);
+    }
+}
